8d3a0800a554d30ef569cfa2cde39f8c30aea20d
Yuto <utomatsu@gmail.com>
2022-08-28 13:42:40 +0900

    update template

--- atcoder/ABC_/D.cpp
+++ atcoder/ABC_/D.cpp
@@ -3,11 +3,8 @@ using namespace std;
 #define _USE_MATH_DEFINES
 #define ll long long int
 
-// コンパイル時に
-// -lgmpxx -lgmp
-// int => mpz_class
-
 int main(int argc, char **argv) {
-	
+	// cout << fixed << setprecision(10);
+
 	return 0;
 }
--- atcoder/ABC_/run.sh
+++ atcoder/ABC_/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 function usage {
 	cat << EOM
 Usage: $(basename $0) [OPTION]...
--h            Display help
--[A-H] VALUE  compile [A-H].cpp
+    -h          =>  Display help
+    -[A-H] num  =>  VALUE
+        compile [A-H].cpp | ./a.out < input num > output
+		needed file name and input number of test case
 EOM
     exit 1
 }
@@ -11,40 +14,67 @@ EOM
 str="${1:1:2}.cpp"
 
 test(){
-    echo "./a.out < input[1-4] > output"
-    echo "test 1" > output
-    ./a.out < input1 >> output
-    echo "" >> output
-    echo "test 2" >> output
-    ./a.out < input2 >> output
-    echo "" >> output
-    echo "test 3" >> output
-    ./a.out < input3 >> output
-    echo "" >> output
-    echo "test 4" >> output
-    ./a.out < input4 >> output
+    # echo "./a.out < input[1-$num] > output"
+    if [ $num -ge 1 ]; then
+        echo "test 1" > output
+        echo "./a.out < input1 >> output"
+        ./a.out < input1 >> output
+        echo "" >> output
+    fi
+    if [ $num -ge 2 ]; then
+        echo "test 2" >> output
+        echo "./a.out < input2 >> output"
+        ./a.out < input2 >> output
+        echo "" >> output
+    fi
+    if [ $num -ge 3 ]; then
+        echo "test 3" >> output
+        echo "./a.out < input3 >> output"
+        ./a.out < input3 >> output
+        echo "" >> output
+    fi
+    if [ $num -ge 4 ]; then
+        echo "test 4" >> output
+        echo "./a.out < input4 >> output"
+        ./a.out < input4 >> output
+    fi
+    echo "finish"
 }
 
 compile(){
     echo "g++ -W -w $str"
     g++ -W -w $str
-    test
-    echo ""
-    # echo "\`\`\`"
-    # cat ./output
-    # echo "\`\`\`"
-    # echo "Done!"
 }
 
-# while getopts ":A:B:C:D:E:h" opt
-while getopts ":ABCDEFHh" opt
+while getopts A:B:C:D:E:F:G:H:h opt
 do
     case $opt in
-        [A-Z] ) compile ;;
-        \? ) usage ;;  # ?: オプションがgetoptsの第一引数に含まれていない場合
-        '-h'|'--help'|* ) usage ;;  # h: ヘルプを表示
+        [A-Z] )
+            compile;
+            num=$OPTARG;
+            # echo `値は $num です`;
+        test $num;;
+        \? )
+        usage;;  # ?: オプションがgetoptsの第一引数に含まれていない場合
+        '-h'|'--help'|* )
+        usage;;  # h: ヘルプを表示
         # * ) usage ;;
     esac
 done
 
 shift $((OPTIND - 1))
+
+
+# 比較演算子
+# 数値1 -eq 数値2	数値1と数値2が等しければ真（=）
+# 数値1 -ne 数値2	数値1と数値2が等しくなければ真（!=、<>）
+# 数値1 -gt 数値2	数値1が数値2よりも大きければ真（>）
+# 数値1 -ge 数値2	数値1が数値2以上であれば真（>=）
+# 数値1 -lt 数値2	数値1が数値2よりも小さければ真（<）
+# 数値1 -le 数値2	数値1が数値2以下であれば真（<=）
+# https://www.pasonatech.co.jp/workstyle/column/detail.html?p=7450
+
+# 引数と変数
+# https://shellscript.sunone.me/parameter.html
+# https://shellscript.sunone.me/variable.html
+# getopt の ":"は値を必要とする引数に付ける
